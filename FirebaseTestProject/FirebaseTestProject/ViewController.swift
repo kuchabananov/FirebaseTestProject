fb1fa96f99417db5debc3a3d46f623686275d5c2
kuchabananov <kuchabananov19@gmail.com>
2021-11-28 16:52:02 +0300

    project created

---
+++ FirebaseTestProject/FirebaseTestProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirebaseTestProject
+//
+//  Created by Евгений on 28.11.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
